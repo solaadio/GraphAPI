9b441968a0bb79c3eb16f6b46b4b8d7294f6b191
Mayur Tendulkar <tendulkar@outlook.com>
2017-05-31 11:29:36 +0530

    UWP App Working
    UWP app logins, sends email as well as logs out.

--- GraphAPI/Leaver/Leaver/App.xaml.cs
+++ GraphAPI/Leaver/Leaver/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,14 @@ namespace Leaver
 {
     public partial class App : Application
     {
+        public static PublicClientApplication IdentityClientApp = null;
+        public static string ClientID = "7214e6cd-85ad-4433-9d13-f2631e1d4142";
+        public static string[] Scopes = { "User.Read", "User.ReadBasic.All ", "Mail.Send" };
+        public static UIParent UiParent = null;
         public App()
         {
             InitializeComponent();
-
+            IdentityClientApp = new PublicClientApplication(ClientID);
             MainPage = new Leaver.MainPage();
         }
 
--- GraphAPI/Leaver/Leaver/MainPage.xaml.cs
+++ GraphAPI/Leaver/Leaver/MainPage.xaml.cs
@@ -1,17 +1,145 @@
-﻿using System;
+﻿using Microsoft.Graph;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Leaver
 {
+
     public partial class MainPage : ContentPage
     {
+        GraphServiceClient client;
+        DirectoryObject Manager;
+        DirectoryObject Me;
+        bool UserExists;
         public MainPage()
         {
             InitializeComponent();
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (App.IdentityClientApp.Users.Count() > 0)
+            {
+                Authenticate.Text = "Sign Out";
+                UserExists = true;
+            }
+            else
+            {
+                Authenticate.Text = "Sign In";
+                UserExists = false;
+            }
+        }
+        private async Task CreateGraphClient()
+        {
+            client = new GraphServiceClient("https://graph.microsoft.com/v1.0",
+                     new DelegateAuthenticationProvider(
+                         async (requestMessage) =>
+                         {
+                             var tokenRequest = await App.IdentityClientApp.AcquireTokenAsync(App.Scopes, App.UiParent).ConfigureAwait(false);
+                             requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", tokenRequest.AccessToken);
+                         }));
+            Me = await client.Me.Request().GetAsync();
+            Username.Text = $"Welcome {((User)Me).DisplayName}";
+        }
+
+        private async void Authenticate_Clicked(object sender, EventArgs e)
+        {
+            if (!UserExists)
+            {
+                await CreateGraphClient();
+                Authenticate.Text = "Signed In";
+                UserExists = true;
+            }
+            else
+            {
+                Authenticate.Text = "Sign Out";
+                App.IdentityClientApp.Remove(App.IdentityClientApp.Users.FirstOrDefault());
+                UserExists = false;
+            }
+        }
+
+        private async void FullDayLeave_Clicked(object sender, EventArgs e)
+        {
+            if (!UserExists)
+                await CreateGraphClient();
+            client = new GraphServiceClient("https://graph.microsoft.com/v1.0",
+                      new DelegateAuthenticationProvider(
+                          async (requestMessage) =>
+                          {
+                              var tokenRequest = await App.IdentityClientApp.AcquireTokenSilentAsync(App.Scopes, App.IdentityClientApp.Users.FirstOrDefault());
+                              requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", tokenRequest.AccessToken);
+                          }));
+
+            Manager = await client.Me.Manager.Request().GetAsync();
+            Me = await client.Me.Request().GetAsync();
+
+            var email = new Message
+            {
+                ToRecipients = new List<Recipient>() { new Recipient() { EmailAddress = new EmailAddress() { Address = ((User)Me).Mail } } },
+                CcRecipients = new List<Recipient>() { new Recipient() { EmailAddress = new EmailAddress() { Address = ((User)Me).Mail } } },
+                Subject = "[Leaver] On Half Day Leave",
+                Body = new ItemBody
+                {
+                    Content = "Hello, <br/>" +
+                             "Today, I'll be working half day today. For urgent matters please call me on my cell. <br/>" +
+                             "Thanks. <br/>" +
+                             ((User)Me).DisplayName,
+                    ContentType = BodyType.Html
+                }
+            };
+
+            SendEmail(email);
+
+        }
+
+     
+        private async void TimeOffForSometimes_Clicked(object sender, EventArgs e)
+        {
+            if (!UserExists)
+                CreateGraphClient();
+            client = new GraphServiceClient("https://graph.microsoft.com/v1.0",
+                      new DelegateAuthenticationProvider(
+                          async (requestMessage) =>
+                          {
+                              var tokenRequest = await App.IdentityClientApp.AcquireTokenSilentAsync(App.Scopes, App.IdentityClientApp.Users.FirstOrDefault());
+                              requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", tokenRequest.AccessToken);
+                          }));
+
+            Manager = await client.Me.Manager.Request().GetAsync();
+            Me = await client.Me.Request().GetAsync();
+
+            var email = new Message
+            {
+                ToRecipients = new List<Recipient>() { new Recipient() { EmailAddress = new EmailAddress() { Address = ((User)Me).Mail } } },
+                CcRecipients = new List<Recipient>() { new Recipient() { EmailAddress = new EmailAddress() { Address = ((User)Me).Mail } } },
+                Subject = "[Leaver] On Half Day Leave",
+                Body = new ItemBody
+                {
+                    Content = "Hello, <br/>" +
+                             "Today, I'll be taking full day leave as I'm not feeling well. For urgent matters please call me on my cell. <br/>" +
+                             "Thanks. <br/>" +
+                             ((User)Me).DisplayName,
+                    ContentType = BodyType.Html
+                }
+            };
+
+            SendEmail(email);
+        }
+
+        private async void SendEmail(Message message)
+        {
+            if (!UserExists)
+                CreateGraphClient();
+            var req = client.Me.SendMail(message);
+            await req.Request().PostAsync();
+            Status.Text = "Email sent to your manager, CC: you";
+        }
+
     }
 }
